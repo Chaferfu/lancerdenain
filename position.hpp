9e55628262848de2ec11ccffe77119bd98ede62b
Brian Martin <bmarti6@a-018818.ups.u-psud.fr>
2017-11-29 15:12:01 +0100

    tjhr

---
+++ position.hpp
@@ -0,0 +1,13 @@
+#ifndef POSITION_H_INCLUDED
+#define POSITION_H_INCLUDED
+
+class Position{
+private:
+	float x, y, z;
+
+public:
+	Position();
+	Position(float x, float y, float z);
+};
+
+#endif;
