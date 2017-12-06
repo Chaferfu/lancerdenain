4f9ce9a6ff385d4abbe561e5af1f6385e9caf437
Brian Martin <bmarti6@a-018818.ups.u-psud.fr>
2017-12-06 16:56:46 +0100

    bite

--- couleur.hpp
+++ couleur.hpp
@@ -8,6 +8,9 @@ private:
 public:
 	Couleur();
 	Couleur(const int r, const int g, const int b);
+	void setR(const int r) {this->r = r;}
+	void setG(const int g) {this->g = g;}
+	void setB(const int b) {this->b = b;}
 };
 
 #endif
